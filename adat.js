204671e1e5f2efade0b30560ff0f31fdd92ab6be
Klaszlo5 <94529898+Klaszlo5@users.noreply.github.com>
2023-03-29 09:24:48 +0200

    sima_sorok

--- adat.js
+++ adat.js
@@ -1,6 +1,4 @@
-
-
-export const ADATLISTA = [
+export	const ADATLISTA = [
     {
         név:"Dézi",
         fajta:"keverék",
@@ -14,51 +12,11 @@ export const ADATLISTA = [
     {
         név:"szuli",
         fajta:"terrier",
-        év:"12"
+        év:"13"
     },
     {
         név:"jnoi",
         fajta:"west",
-        év:"12"
+        év:"17"
     }
-]
-
-function listakeveres(lista) {
-  let keverlistaall = [];
-  let szamok;
-  while (keverlistaall.length < lista.length) {
-    szamok = Math.floor(Math.random() * lista.length);
-    let index = 0;
-    while (
-      index < keverlistaall.length &&
-      !(szamok == keverlistaall[index])
-    ) {
-      index++;
-    }
-    if (index >= keverlistaall.length) {
-      keverlistaall.push(szamok);
-    }
-  }
-}
-
-$(function(){
-    console.log(ADATLISTA)
-})
-function tablacsinal(tableData) {
-    var tabla = document.createElement('table');
-    
-}
-function rendezes1(lista) {
-  lista.sort();
-}
-
-tablacsinal([[ADATLISTA]]);
-
-export function szuresfajta(kulcs,szures){
-    const SZURTLISTA=ADATLISTA.filter(function(){
-        szures=0;
-        return a[kulcs]===szuresifeltetel;
-
-    });
-    return SZURTLISTA
-}
+];
